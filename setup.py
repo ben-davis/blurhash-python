9c97178945beee8363d04488fb9ff41c4180e5bf
Ben Davis <ben@bencdavis.com>
2023-02-27 14:30:23 -0500

    Replace PIL with pyvips

--- setup.py
+++ setup.py
@@ -3,51 +3,50 @@ from __future__ import absolute_import
 
 from setuptools import setup
 
-
-with open('README.md', 'r') as readme_file:
+with open("README.md", "r") as readme_file:
     long_description = readme_file.read()
 
 
 tests_require = [
-    'pytest',
+    "pytest",
 ]
 
 setup(
-    name='blurhash-python',
-    description='BlurHash encoder implementation for Python',
+    name="blurhash-python",
+    description="BlurHash encoder implementation for Python",
     long_description=long_description,
-    long_description_content_type='text/markdown',
-    url='https://blurha.sh',
+    long_description_content_type="text/markdown",
+    url="https://blurha.sh",
     use_scm_version=dict(
-        write_to='src/blurhash/_version.py',
+        write_to="src/blurhash/_version.py",
     ),
-    author='Atte Lautanala',
-    author_email='atte.lautanala@wolt.com',
-    packages=['blurhash'],
-    package_dir={'': 'src'},
+    author="Atte Lautanala",
+    author_email="atte.lautanala@wolt.com",
+    packages=["blurhash"],
+    package_dir={"": "src"},
     install_requires=[
-        'cffi',
-        'Pillow',
-        'six',
+        "cffi",
+        "pyvips",
+        "six",
     ],
     setup_requires=[
-        'cffi',
-        'setuptools-scm',
+        "cffi",
+        "setuptools-scm",
     ],
-    cffi_modules=['src/build_blurhash.py:ffibuilder'],
+    cffi_modules=["src/build_blurhash.py:ffibuilder"],
     tests_require=tests_require,
     extras_require={
-        'testing': tests_require,
+        "testing": tests_require,
     },
     classifiers=[
-        'Development Status :: 5 - Production/Stable',
-        'License :: OSI Approved :: MIT License',
-        'Operating System :: OS Independent',
-        'Programming Language :: Python :: 3',
-        'Programming Language :: Python :: 3.6',
-        'Programming Language :: Python :: 3.7',
-        'Programming Language :: Python :: 3.8',
-        'Programming Language :: Python :: 3.9',
-        'Programming Language :: Python :: 3.10',
+        "Development Status :: 5 - Production/Stable",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3.9",
+        "Programming Language :: Python :: 3.10",
     ],
 )
